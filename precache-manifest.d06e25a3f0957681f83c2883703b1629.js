4286c03e5a3922bb40376391ce4421be654d6902
Fehmi Nechba <nechba.fehmi@gmail.com>
2023-02-20 21:45:13 +0100

    Updates

---
+++ precache-manifest.d06e25a3f0957681f83c2883703b1629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94be28593e0c7d7ef59b",
+    "url": "/RecipeFood/static/css/main.a6266e71.chunk.css"
+  },
+  {
+    "revision": "94be28593e0c7d7ef59b",
+    "url": "/RecipeFood/static/js/main.94be2859.chunk.js"
+  },
+  {
+    "revision": "e391f9b210cbefed7347",
+    "url": "/RecipeFood/static/js/1.e391f9b2.chunk.js"
+  },
+  {
+    "revision": "105065e2fa33301e30d5",
+    "url": "/RecipeFood/static/js/runtime~main.105065e2.js"
+  },
+  {
+    "revision": "af40961e254fd71218bfae5ad9cc3601",
+    "url": "/RecipeFood/index.html"
+  }
+];
